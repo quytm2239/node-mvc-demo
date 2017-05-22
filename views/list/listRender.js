4e51aa8cc84dd25e20661f78a8e48a900c8bd3e3
Tran Manh Quy <quytm2239@gmail.com>
2017-05-22 15:40:06 +0700

    Update logic

--- views/list/listRender.js
+++ views/list/listRender.js
@@ -4,4 +4,12 @@ module.exports = function(app, views_router, config){
 	views_router.get('/list', function(req, res) {
         res.sendFile(__dirname + '/list.html');
 	});
+
+	views_router.get('/css/list.css', function(req, res) {
+		res.sendFile(__dirname + '/css/list.css');
+	});
+
+	views_router.get('/js/list.js', function(req, res) {
+		res.sendFile(__dirname + '/js/list.js');
+	});
 };
--- views/main/mainRender.js
+++ views/main/mainRender.js
@@ -5,4 +5,12 @@ module.exports = function(app, views_router, config){
 	views_router.get('/main', function(req, res) {
         res.sendFile(__dirname + '/main.html');
 	});
+
+	views_router.get('/css/main.css', function(req, res) {
+		res.sendFile(__dirname + '/css/main.css');
+	});
+
+	views_router.get('/js/main.js', function(req, res) {
+		res.sendFile(__dirname + '/js/main.js');
+	});
 };
